ec64a8ebdd54cbc840b82f0a3a35db5af2c2919a
michellewkx <371640895@qq.com>
2017-01-15 17:12:02 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: michellewkx
+* @Date:   2017-01-15 17:01:44
+* @Last Modified by:   michellewkx
+* @Last Modified time: 2017-01-15 17:06:05
+*/
+
+'use strict';
+var server = require('./server');
+server.start();
