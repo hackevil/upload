91578b3b05ea4b7e2d57802252d83799bf47381f
michellewkx <371640895@qq.com>
2017-01-18 01:09:06 +0800

    commit and upload

--- router.js
+++ router.js
@@ -2,14 +2,14 @@
 * @Author: michellewkx
 * @Date:   2017-01-17 15:27:11
 * @Last Modified by:   michellewkx
-* @Last Modified time: 2017-01-18 00:37:34
+* @Last Modified time: 2017-01-18 00:52:27
 */
 
 'use strict';
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
 	console.log('About to route a request for ' + pathname);
 	if (typeof handle[pathname] === 'function')  {
-		handle[pathname](response);
+		handle[pathname](response, postData);
 	}
 	else {
 		console.log("No request handler found for" + pathname);
--- server.js
+++ server.js
@@ -2,16 +2,27 @@
 * @Author: michellewkx
 * @Date:   2017-01-09 20:04:57
 * @Last Modified by:   michellewkx
-* @Last Modified time: 2017-01-18 00:41:10
+* @Last Modified time: 2017-01-18 00:54:59
 */
 
 var http = require("http");
 var url = require("url");
 function start(route, handle) {
 	    function onRequest(request, response) {
-	      var path = url.parse(request.url).pathname;
-		  console.log("Request for " + path + " received");
-		  route(handle, path, response);
+	        var path = url.parse(request.url).pathname;
+	        var postData = '';
+		    console.log("Request for " + path + " received");
+		    request.setEncoding("utf8");
+
+		    request.addListener("data", function(postDataChunk) {
+		      postData += postDataChunk;
+		      console.log("Received POST data chunk '"+
+		      postDataChunk + "'.");
+		    });
+
+		    request.addListener("end", function() {
+		      route(handle, path, response, postData);
+		    });
 		}
 	  http.createServer(onRequest).listen(8888);
 	  console.log("Server started.");
